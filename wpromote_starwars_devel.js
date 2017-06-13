350891359f133a0dbd8ca119fd703b0d93bf4ebe
seanwestfall <sean.westfall@gmail.com>
2017-06-13 09:06:19 -0700

    Project init

---
+++ wpromote_starwars_devel.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 8080;
+
+app.use(express.static('assets'));
+
+app.get('/', function (req, res) {
+   res.sendFile( __dirname + "index.html" );
+})
+
+var server = app.listen(port, function () {
+   var host = server.address().address
+   //var port = server.address().port
+
+   console.log("wpromote star wars front end project listening at http://%s:%s", host, port)
+
+})
